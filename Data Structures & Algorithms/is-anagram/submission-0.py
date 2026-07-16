db7934278dc580c4971885d39191e92180558bbd
Thomas Shephard <98121543+TomShephard@users.noreply.github.com>
2026-07-16 15:09:59 +0100

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        print (ord("a"))
+        if len(s) != len(t):
+            return False
+
+        sMap = {}
+        tMap = {}
+    
+        for x in s:
+            if x in sMap:
+                sMap[x] += 1
+            else:
+                sMap[x] = 1
+        
+        for x in t:
+            if x in tMap:
+                tMap[x] += 1
+            else:
+                tMap[x] = 1
+        
+        if tMap == sMap:
+            return True
+        else:
+            return False
+        
+#
+        

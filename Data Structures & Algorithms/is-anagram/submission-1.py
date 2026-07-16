726ece42b6b409e5eac3cea39f05b07271aebec7
Thomas Shephard <98121543+TomShephard@users.noreply.github.com>
2026-07-16 15:23:23 +0100

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        sMap = {}
+        tMap = {}
+
+        for x in s:
+            if x in sMap:
+                sMap[x] += 1
+            else:
+                sMap[x] = 1
+        
+        for x in t:
+            if x in tMap:
+                tMap[x] += 1
+            else:
+                tMap[x] = 1
+
+        if tMap == sMap:
+            return True
+        else:
+            return False
+        
+
+        

e3cc828fc496bfee97d6619da7f2c22cbc131579
Thomas Shephard <98121543+TomShephard@users.noreply.github.com>
2026-08-02 16:42:27 +0100

    Add: is-anagram - submission-24

---
+++ Data Structures & Algorithms/is-anagram/submission-24.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        smap = {}
+        tmap = {}
+
+        for c in s:
+            smap[c] = 1 + smap.get(c, 0)
+        
+        for c in t:
+            tmap[c] = 1 + tmap.get(c, 0)
+
+        if smap == tmap:
+            return True
+        else:
+            return False
